a131742622bd10121756db8cfcebb97e40b90d24
VeeGee-Creations <vgeesman@gmail.com>
2021-05-01 00:52:02 -0700

    await populate

--- index.js
+++ index.js
@@ -91,14 +91,14 @@ app.get('/directors/:Name', passAuth, (req, res) => {
 // get user profile
 
 app.get('/users/profile', passAuth, (req, res) => {
-    Users.findOne({Username: req.user.Username})
-    .populate([{
+    await Users.findOne({Username: req.user.Username})
+    .populate({
         path: 'Favorites', model: Movies,
         populate: [
             {path: 'Directors', model: Directors},
             {path: 'Genres', model: Genres}
         ]
-    }])
+    })
     .then((user) => res.json(user))
     .catch((err) => res.status(500).send(`Error: ${err}`));
 });
