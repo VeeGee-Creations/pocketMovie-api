c25839efe08d8e6abb22aaa8df7fe060bbf3915f
VeeGee-Creations <vgeesman@gmail.com>
2021-04-30 20:28:02 -0700

    fixed profile only returning 1 favorite

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.get('/directors/:Name', passAuth, (req, res) => {
 // get user profile
 
 app.get('/users/profile', passAuth, (req, res) => {
-    Users.findOne({Username: req.user.Username})
+    Users.find({Username: req.user.Username})
     .populate({
         path: 'Favorites', model: Movies,
         populate: [{path: 'Directors', model: Directors}, {path: 'Genres', model: Genres}]
