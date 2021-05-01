e5581978928518ad7bbb834cbe21bdc79b5a9023
VeeGee-Creations <vgeesman@gmail.com>
2021-04-30 20:37:28 -0700

    add array to population

--- index.js
+++ index.js
@@ -92,10 +92,10 @@ app.get('/directors/:Name', passAuth, (req, res) => {
 
 app.get('/users/profile', passAuth, (req, res) => {
     Users.find({Username: req.user.Username})
-    .populate({
+    .populate([{
         path: 'Favorites', model: Movies,
         populate: [{path: 'Directors', model: Directors}, {path: 'Genres', model: Genres}]
-    })
+    }])
     .then((user) => res.json(user))
     .catch((err) => res.status(500).send(`Error: ${err}`));
 });
