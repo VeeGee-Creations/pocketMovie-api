fab8553df1f3afacb9eb0cfa301f75b2d8808da5
VeeGee-Creations <vgeesman@gmail.com>
2021-05-09 02:58:47 -0700

    enable cors

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ const corsOptions = {
 }
 
 //middleware
+app.use(cors(corsOptions));
 app.use(morgan('common'));
 app.use(express.static('public'));
 app.use(express.json());
-// app.use(cors(corsOptions));
 
 mongoose.connect(process.env.CONNECTION_URI, {
 useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
